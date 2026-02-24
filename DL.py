Integrated backpropagation to classes & fixed functionality for multi-layer networks

DL.py
@@ -2,17 +2,36 @@ import numpy as np
 
 class Linear:
     def __init__(self, input_size: int, output_size: int, lr=0.1):
-        self.shape = (input_size, output_size)
-        self.W = np.random.randn(input_size, output_size) * np.sqrt(2 / input_size)
-        self.B = np.zeros(output_size)
+        self.shape = (output_size, input_size)
+        self.W = np.random.randn(output_size, input_size) * np.sqrt(2 / input_size)
+        self.B = np.zeros(output_size).reshape(-1, 1)
+
+        self.grad = 0
 
         # Learning Rate
         self.lr = lr
 
     def forward(self, X):
-        self.X = X
-        self.Z = X @ self.W + self.B
-        return np.maximum(0, self.Z)
+        self.X = X.reshape(-1, 1)
+        self.Z = self.W @ self.X + self.B
+        return np.maximum(0, self.Z) #RELU
+    
+    def backward(self, incoming_grad):
+        # (dl/dy * dy/dz)
+        d_relu = (self.Z > 0).astype(float)
+        d_relu = incoming_grad * d_relu
+
+        # Bias delta
+        self.grad_B = d_relu
+        # (dl/dW)
+        self.grad_W = d_relu @ self.X.T
+
+        return self.W.T @ d_relu
+    
+    def grad_descent(self):
+        # ???
+        self.W -= self.lr * self.grad_W
+        self.B -= self.lr * self.grad_B
 
 class NN:
     def __init__(self, input_size: int):
@@ -33,6 +52,15 @@ class NN:
         for layer in self.layers:
             X = layer.forward(X)
         return X
+    
+    def backward(self, x):
+        for i in range(len(self.layers) - 1, -1, -1):
+            layer = self.layers[i]
+            x = layer.backward(x)
+    
+    def grad_descent(self):
+        for layer in self.layers:
+            layer.grad_descent()
     
     def __str__(self):
         count = 1

andgate.py
@@ -6,12 +6,17 @@ start_time = time.time()
 
 n_samples = 1000
 input_dim = 5
-lr = 0.01
-layer = Linear(input_dim, 1, lr=lr)
+lr = 0.001
+layer = Linear(input_dim, 2, lr=lr)
+layertwo = Linear(2, 1, lr=lr)
+
+model = NN(input_dim)
+model.add_layer(layer)
+model.add_layer(layertwo)
 
 X_train = np.random.randint(0, 2, size=(n_samples, input_dim)).astype(float)
 y_train = ((X_train[:, 0] == 1) & (X_train[:, 4] == 1)).astype(float)
-
+loss = 0
 for epoch in range(50000):
     # Pick a random sample
     i = np.random.randint(0, n_samples)
@@ -19,25 +24,34 @@ for epoch in range(50000):
     target = y_train[i]
 
     # Forward
-    output = layer.forward(x)
+    output = model.forward(x)
+    loss += 0.5 * (output - target)**2
+
+    if epoch % 5000 == 0:
+        loss = loss / epoch
+        print(f"Epoch {epoch}, Avg Loss: {loss[0][0]}")
+        loss = 0
         
     # Loss
-    error = output - target
-        
-    # Backprop
-    layer.W -= lr * (x.T @ error)
-    layer.B -= lr * error.flatten()
+    d_loss = output - target
 
-print("--- TRAINED WEIGHTS ---")
-for i, weight in enumerate(layer.W.flatten()):
-    print(f"Weight for X{i}: {weight:.4f}")
+    # Backprop
+    model.backward(d_loss)
+
+    # Gradient Descent
+    model.grad_descent()
+
 print("\n--- TEST: Does it ignore noise? ---")
 test_clean = np.array([1, 0, 0, 0, 1])
 test_noisy = np.array([1, 1, 1, 1, 1])
 test_false = np.array([1,1,0,1,0])
+test_falsetwo = np.array([0,1,0,1,1])
 
-print(f"Clean Input [1,0,0,0,1] -> Output: {layer.forward(test_clean)}")
-print(f"Noisy Input [1,1,1,1,1] -> Output: {layer.forward(test_noisy)}")
-print(f"Noisy Input [1,1,0,1,0] -> Output: {layer.forward(test_false)}")
+print(f"Clean Input [1,0,0,0,1] -> Output: {model.forward(test_clean)}")
+print(f"Noisy Input [1,1,1,1,1] -> Output: {model.forward(test_noisy)}")
+print(f"Noisy Input [1,1,0,1,0] -> Output: {model.forward(test_false)}")
+print(f"Noisy Input [0,1,0,1,1] -> Output: {model.forward(test_falsetwo)}")
 
 print("--- %s seconds ---" % (time.time() - start_time))
+
+print(model)